829cc5e98bb42f2d7775f13c8469b9d98ef043bc
ivanburda1986 <burda.ivan@gmail.com>
2021-03-17 23:11:17 +0100

    redirects copy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWepackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require ('copy-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
@@ -18,7 +19,10 @@ module.exports = {
   plugins: [
     new HtmlWepackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin([
+      {from: '_redirects'}
+    ])
   ],
   devServer: {
     historyApiFallback: true,
